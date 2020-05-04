fba234c0edda8645ec38da5fb9972af2a1c8f0a1
Gautham Rajesh <gauthamemail@gmail.com>
2020-05-04 15:24:51 -0700

    Update #1

--- model.py
+++ model.py
@@ -4,6 +4,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
+
 class Actor(nn.Module):
     def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
         super(Actor, self).__init__()
@@ -11,11 +16,17 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
+        self.reset_parameters()
 
     def forward(self, state):
         x = F.leaky_relu(self.fc1(state), negative_slope=0.01)
         x = F.leaky_relu(self.fc2(x), negative_slope=0.01)
         return F.tanh(self.fc3(x))
+    
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
 
 class Critic(nn.Module):
@@ -25,9 +36,15 @@ class Critic(nn.Module):
         self.fc1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units + action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
+        self.reset_parameters()
 
     def forward(self, state, action):
         x = F.leaky_relu(self.fc1(state), negative_slope=0.01)
         x = torch.cat((x, action), dim=1)
         x = F.leaky_relu(self.fc2(x), negative_slope=0.01)
         return self.fc3(x)
+    
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
